071c82617ba10f210b70b161b689a8f261d26745
themudhaxk <156187728+themudhaxk@users.noreply.github.com>
2024-08-06 10:30:56 +0100

    Update constants.js

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,7 @@
 export const BASE_URL = "https://foodilystore.onrender.com";
-export const USERS_URL = "https://foodilystore.onrender.com/api/users";
-export const CATEGORY_URL = "https://foodilystore.onrender.com/api/category";
-export const PRODUCT_URL = "https://foodilystore.onrender.com/api/products";
-export const UPLOAD_URL = "https://foodilystore.onrender.com/api/upload";
-export const ORDERS_URL = "https://foodilystore.onrender.com/api/orders";
-export const PAYSTACK_URL = "https://foodilystore.onrender.com/api/paystack";
+export const USERS_URL = "/api/users";
+export const CATEGORY_URL = "/api/category";
+export const PRODUCT_URL = "/api/products";
+export const UPLOAD_URL = "/api/upload";
+export const ORDERS_URL = "/api/orders";
+export const PAYSTACK_URL = "/api/paystack";
