94e7a6a9f668db42e1e6290ba497e7539762c780
themudhaxk <156187728+themudhaxk@users.noreply.github.com>
2024-08-06 15:43:00 +0100

    Update createToken.js

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
     res.cookie("jwt", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // Set to true if in production
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 30 * 24 * 60 * 60 * 1000,
     })
 
