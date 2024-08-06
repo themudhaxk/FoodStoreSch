7da35b493d0273f2a15bc7b2b54093d159b74344
themudhaxk <156187728+themudhaxk@users.noreply.github.com>
2024-08-06 10:34:17 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Customize chunking here
+          // For example:
+          // vendor: ['react', 'react-dom'],
+        },
+      },
+      chunkSizeWarningLimit: 1000, // Adjust chunk size limit
+    },
+  },
 })
+
