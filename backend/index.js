c90c719436d62b03d965ff1dc480cde5f9281c89
themudhaxk <156187728+themudhaxk@users.noreply.github.com>
2024-08-06 12:30:44 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.json());
 //app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-  origin: '*',
+  origin: 'https://foodstoresch.onrender.com',
 }));
 
 
